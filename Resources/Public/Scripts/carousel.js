6fa4185d79e2275b00e18761a10ebcb0d53eef01
Kerstin Huppenbauer <k.huppenbauer@web.de>
2015-02-22 11:33:22 +0100

    [TASK] move navigation into the carousel slides

--- Resources/Public/Scripts/carousel.js
+++ Resources/Public/Scripts/carousel.js
@@ -5,6 +5,10 @@ var OwlCarousel = function () {
             var owl = jQuery("#" + id).owlCarousel({
                 navigation : navigation, // Show next and prev buttons
                 pagination : pagination,
+                navigationText: [
+                    "<a class='left carousel-control' role='button' data-slide='prev'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span><span class='sr-only'>Previous</span></a>",
+                    "<a class='right carousel-control' role='button' data-slide='next'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span><span class='sr-only'>Next</span></a>"
+                ],
                 autoPlay : autoPlay,
                 stopOnHover : stopOnHover,
                 items: items,
